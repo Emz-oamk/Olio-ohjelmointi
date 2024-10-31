a2dd7c5286b89215989bd4139d1c18384a2113bc
Emma <t3taem00@students.oamk.fi>
2024-10-31 22:03:05 +0200

    H1 valmis

--- H1/main.cpp
+++ H1/main.cpp
@@ -2,20 +2,19 @@
 
 using namespace std;
 
-void calcSum(int, int);
+void retSum(int, int);
 int main()
 {
-    cout << "Hello World!" << endl;
     int a,b;
     cout << "Anna luku" << endl;
     cin >> a;
     cout << "Anna toinen luku" << endl;
     cin >> b;
-    calcSum(a,b);
+    retSum(a,b);
     return 0;
 }
 
-void calcSum(int x,int y){
-    cout << "Lukujen summa on " << x+y << endl;
+void retSum(int x,int y){
+    cout << "Lukujen jako on " << x/y << endl;
 
 }
